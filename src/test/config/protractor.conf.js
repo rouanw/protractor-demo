267214b94c5918a95cc965949439bb81342fcb8a
Rouan Wilsenach <rouanw@gmail.com>
2014-10-13 13:25:29 +0200

    stripped out some unneeded config and updated some package versions

--- src/test/config/protractor.conf.js
+++ src/test/config/protractor.conf.js
@@ -1,17 +1,5 @@
 exports.config = {
+    chromeOnly: true,
     chromeDriver: '../../../node_modules/chromedriver/bin/chromedriver',
-    specs: ['../functional/*.js'],
-    allScriptsTimeout: 20000,
-    seleniumServerJar: '../../../node_modules/selenium-server/lib/runner/selenium-server-standalone-2.35.0.jar',
-
-    splitTestsBetweenCapabilities: true,
-    multiCapabilities: [{
-        browserName: 'chrome',
-        count: 1
-    }],
-
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 70000
-    }
+    specs: ['../functional/*.js']
 };
