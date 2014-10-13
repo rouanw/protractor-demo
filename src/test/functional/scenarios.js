a49cdb82101ce6daa4f7b45d7568593cee64e475
Rouan Wilsenach <rouanw@gmail.com>
2014-10-13 14:56:15 +0200

    a simple test to see if the protractor github page tops the google search rankings

--- src/test/functional/scenarios.js
+++ src/test/functional/scenarios.js
@@ -1,13 +1,20 @@
 'use strict';
 
-describe('my tests', function() {
+describe('protractor github page', function() {
 
   beforeEach(function(){
     browser.ignoreSynchronization = true;
   });
 
-  it('should do stuff', function() {
+  it('should be the first google search result', function() {
     browser.get('http://www.google.co.za');
+    element(by.id('gbqfq')).sendKeys('protractor github');
+    element(by.id('gbqfb')).click();
+    browser.wait(function () {
+      return browser.isElementPresent(by.css('h3.r a'))
+    }, 1000);
+    element(by.css('h3.r a')).click();
+    expect(element(by.css('.repository-description p')).getText()).toBe('E2E test framework for Angular apps');
   });
 
 });
