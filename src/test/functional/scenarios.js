0a68cf296a2fa7dd2b15c9164f948064ac9ce1ed
Rouan <rmw@thoughtworks.com>
2015-07-27 12:46:41 +0200

    fix selectors

--- src/test/functional/scenarios.js
+++ src/test/functional/scenarios.js
@@ -8,13 +8,13 @@ describe('protractor github page', function() {
 
   it('should be the first google search result', function() {
     browser.get('http://www.google.co.za');
-    element(by.id('gbqfq')).sendKeys('protractor github');
-    element(by.id('gbqfb')).click();
+    element(by.css('input[title="Search"]')).sendKeys('protractor github');
+    element(by.css('button[value="Search"]')).click();
     browser.wait(function () {
       return browser.isElementPresent(by.css('h3.r a'))
     }, 1000);
     element(by.css('h3.r a')).click();
-    expect(element(by.css('.repository-description p')).getText()).toBe('E2E test framework for Angular apps');
+    expect(element(by.css('.repository-description')).getText()).toBe('E2E test framework for Angular apps');
   });
 
 });
